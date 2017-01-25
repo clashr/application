f1356f1f669ac7817e02c2b973e235a747cfc985
Spencer Kee <spencerkee7@gmail.com>
2017-01-25 18:23:20 -0500

    Message publishing

--- db/migrate/20170125223204_add_columns_to_contest.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToContest < ActiveRecord::Migration[5.0]
-  def change
-    add_column :contests, :Timeout, :integer
-    add_column :contests, :MemLimit, :integer
-    add_column :contests, :Command, :string
-    add_column :contests, :Stdin, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125223204) do
+ActiveRecord::Schema.define(version: 20170125225550) do
 
   create_table "contests", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "duedate"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
     t.integer  "Timeout"
     t.integer  "MemLimit"
     t.string   "Command"
     t.string   "Stdin"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "submissions", force: :cascade do |t|
